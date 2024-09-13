08b3db44973c04cdd36da8bc07d42817a4a2357a
Evaristo <114101006+EvaristoAguirre@users.noreply.github.com>
2024-09-13 20:56:20 -0300

    bug variables locales

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -1,3 +1,6 @@
-import { createClient} from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 
-export const Supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_KEY);
+export const Supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_KEY
+);
